aaba68195d0c32e0639dfe2c3cbe87cce1b2d89f
Emma Faye <emzfaye@gmail.com>
2020-04-05 11:19:06 -0700

    Converted CRLF to LF and fixed some dev settings

--- shell.sh
+++ shell.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -it --rm -v "S:\Projects\Websites & Brands\ssbu-match-analyzer:/usr/src/app" --name ssbu-match-analyzer-container ssbu-match-analyzer
+docker run -it --rm -v "C:\Users\Emberwyn\Documents\Projects\ssbu-match-analyzer:/usr/src/app" --name ssbu-match-analyzer-container ssbu-match-analyzer
