7a8386c4760ebc765f0ba66c2337d2213271086d
Emma Faye <emzfaye@gmail.com>
2020-04-06 19:14:56 -0700

    Updated volume path to use project path

--- shell.sh
+++ shell.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-docker run -it --rm -v "C:\Users\Emberwyn\Documents\Projects\ssbu-match-analyzer:/usr/src/app" --name ssbu-match-analyzer-container ssbu-match-analyzer
+# Gets absolute window path using pwd and turning it into Windows-style syntax for the folder directory (needed by Docker)
+DIR=$(echo "$PWD" | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
+docker run -it --rm -v "$DIR":/usr/src/app --name ssbu-match-analyzer-container ssbu-match-analyzer
